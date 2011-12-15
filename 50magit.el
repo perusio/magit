b434cf2acc70a149bb3ad51c0d88a72876226eca
António P. P. Almeida <appa@perusio.net>
2011-12-15 02:42:40 +0000

    * Updated the control file. Fixed issues with magit-svn on startup file.

--- 50magit.el
+++ 50magit.el
@@ -2,4 +2,5 @@
 (autoload 'magit-status "magit" nil t)
 ;;; Load topgit and git svn interaction.
 (eval-after-load 'magit '(progn (require 'magit-topgit nil t)
-                                (require 'magit-svn nil t)))
+                                        ;(require 'magit-svn nil t)
+                                ))
